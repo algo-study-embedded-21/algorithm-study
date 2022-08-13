df9bbb4dfe9d86412c849659cbbd521018ff0afe
이경훈 <65318329+khlee9606@users.noreply.github.com>
2022-08-13 12:14:44 +0900

    Update week2.3190.cpp

--- kyeonghoon/week2.3190.cpp
+++ kyeonghoon/week2.3190.cpp
@@ -1,53 +1,49 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+int n, k, l, ans,s_x,s_y,s_dir;
+int map[100][100];
+vector<pair<int, char>> dir;
+queue<pair<int, int>> s;
 #define X first
 #define Y second
-int n, k, l, time;
-int s_x, s_y, s_dir;
-int map[100][100];
+
 int dx[] = { 0,1,0,-1 };
 int dy[] = { 1,0,-1,0 };
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
-	cin >> k;
-	queue<pair<int, int>> s;
-	vector<pair<int, char>> dir;
-	for (int i = 0; i < k; i++) {
+	cin >> n >> k;
+	while (k--) {
 		int x, y;
 		cin >> x >> y;
-		x--; y--;
-		map[x][y] = 1;
+		map[x - 1][y - 1] = 1;
 	}
 	cin >> l;
-	for (int i = 0; i < l; i++) {
-		int num; char p;
-		cin >> num >> p;
-		dir.push_back({ num, p });
+	while(l--) {
+		int x; char c;
+		cin >> x >> c;
+		dir.push_back({ x,c });
 	}
-	s.push({ 0, 0 });
+	s.push({ 0,0 });
 	map[0][0] = 2;
 	while (true) {
-		s_x += dx[s_dir];
-		s_y += dy[s_dir];
-		if (s_x < 0 || s_x >= n || s_y < 0 || s_y >= n)break;
-		if (map[s_x][s_y] == 2)break;
-		s.push({ s_x, s_y });
-		if (map[s_x][s_y] == 1) {
-			map[s_x][s_y] = 2;
+		ans++;
+		int nx = s_x + dx[s_dir];
+		int ny = s_y + dy[s_dir];
+		if (nx<0 || nx>=n || ny < 0 || ny >= n)break;
+		if (map[nx][ny] == 2)break;
+		s.push({ nx,ny });
+		if (map[nx][ny] == 1) {
+			map[nx][ny] = 2;
 		}
 		else {
 			map[s.front().X][s.front().Y] = 0;
+			map[nx][ny] = 2;
 			s.pop();
 		}
-		map[s_x][s_y] = 2;
-		time++;
-		for (auto x : dir) {
-			if (x.first == time) {
-				if (x.second == 'D') {
+		for (auto p : dir) {
+			if (p.X == ans) {
+				if (p.Y == 'D') {
 					s_dir = (s_dir + 1) % 4;
 				}
 				else {
@@ -55,7 +51,7 @@ int main() {
 				}
 			}
 		}
+		s_x = nx, s_y = ny;
 	}
-	cout << time + 1 << '\n';
+	cout << ans;
 }
-
