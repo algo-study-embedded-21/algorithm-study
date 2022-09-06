e97d6b8ab84788cc3db71a8ab511b4dd1ec76d4c
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2022-09-07 02:43:41 +0900

    Update 5주차_미세먼지안녕.cpp

--- seungho/5주차_미세먼지안녕.cpp
+++ seungho/5주차_미세먼지안녕.cpp
@@ -33,52 +33,63 @@ void dust() {
 				if (ny<1 || nx<1 || ny>R || nx>C) continue;
 				if (MAP[ny][nx] == -1) continue;
 				MAP2[ny][nx] += scatter;
-				MAP[v[i].y][v[i].x] -= scatter;
+				MAP[i][j] -= scatter;
 			}
 		}
 	}
 	// 확산 이후 결과 계산
 	for (int i = 1; i <= R; i++) {
 		for (int j = 1; j <= C; j++) {
-			if (MAP2[i][j] == 0) continue;
+			/*if (MAP2[i][j] == 0) continue;*/
 			MAP[i][j] += MAP2[i][j];
 			MAP2[i][j] = 0;
+
 		}
+
 	}
 
 	// 반시계 방향 바람
-	int s1y = filter[0].y;
-	int s1x = filter[0].x;
+	int sy1 = filter[0].y-1;
+	int sx1 = filter[0].x;
+	int y1 = sy1;
+	int x1 = sx1;
 	int w1 = 0;
 	while (true) {
-		int ny = s1y + windy[w1];
-		int nx = s1x + windx[w1];
-		if (ny<1 || nx<1 || ny>R || nx>C) {
-			w1++;
+		int ny = y1 + windy[w1];
+		int nx = x1 + windx[w1];
+		if (ny<1 || nx<1 || ny > (sy1+1) || nx>C) {
+			w1++;	
+
 			continue;
 		}
 		if (MAP[ny][nx] == -1) break;
-		MAP[s1y][s1x] = MAP[ny][nx];
-		s1y = ny;
-		s1x = nx;
+
+		MAP[y1][x1] = MAP[ny][nx];
+		y1 = ny;
+		x1 = nx;
 	}
+	MAP[y1][x1] = 0; // 시작부분
+
 	// 시계 방향 바람
-	int s2y = filter[1].y;
-	int s2x = filter[1].x;
+	int sy2 = filter[1].y+1;
+	int sx2 = filter[1].x;
+	int y2 = sy2;
+	int x2 = sx2;
 	int w2 = 2;
 	while (true) {
-		int ny = s2y + windy[w2];
-		int nx = s2x + windx[w2];
-		if (ny<1 || nx<1 || ny>R || nx>C) {
+		int ny = y2 + windy[w2];
+		int nx = x2 + windx[w2];
+		if (ny<(sy2-1) || nx<1 || ny>R || nx>C) {
 			w2--;
 			if (w2 < 0) w2 += 4;
 			continue;
 		}
 		if (MAP[ny][nx] == -1) break;
-		MAP[s2y][s2x] = MAP[ny][nx];
-		s2y = ny;
-		s2x = nx;
+		MAP[y2][x2] = MAP[ny][nx];
+		y2 = ny;
+		x2 = nx;
 	}
+	MAP[y2][x2] = 0;  // 시작부분
 }
 
 
@@ -88,8 +99,7 @@ int main() {
 	for (int i = 1; i <= R; i++) {
 		for (int j = 1; j <= C; j++) {
 			cin >> MAP[i][j];
-			if (MAP[i][j] > 0) v.push_back({ i,j,MAP[i][j] });
-			else if (MAP[i][j] == -1) filter.push_back({ i,j,-1 });
+			if (MAP[i][j] == -1) filter.push_back({ i,j,-1 });
 		}
 	}
 
@@ -100,9 +110,11 @@ int main() {
 	int ans = 0;
 	for (int i = 1; i <= R; i++) {
 		for (int j = 1; j <= C; j++) {
-			if (MAP[i][j] == -1) continue;
+
+			if (MAP[i][j] == -1 || MAP[i][j]==0) continue;
 			ans += MAP[i][j];
 		}
+
 	}
 	cout << ans;
 	return 0;
