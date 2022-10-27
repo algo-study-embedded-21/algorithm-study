bfc151ea184f23ae376e3ede0b06bd5ee12a5c65
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2022-10-27 23:48:14 +0900

    Update 10주차_소코반.cpp

--- seungho/10주차_소코반.cpp
+++ seungho/10주차_소코반.cpp
@@ -12,13 +12,15 @@ int main()
 
 	while (1) {
 		int R, C;
+
+		// 캐릭터 현재 위치
 		int wy = 0;
 		int wx = 0;
-		int MAP2[15][15] = { 0 };
-		int flag2 = 0;
+		int MAP2[20][20] = { 0 };
+		int endgame = 0;
 		cin >> R >> C;
 		if (R == 0 && C == 0) break;
-		string MAP[16];
+		string MAP[20];
 		for (int i = 0; i < R; i++) {
 			cin >> MAP[i];
 		}
@@ -30,24 +32,24 @@ int main()
 					wy = i;
 					wx = j;
 				}
-				else if (MAP[i][j] == '+') MAP2[i][j] = 3;
+				if (MAP[i][j] == '+' || MAP[i][j] == 'W' || MAP[i][j] == 'B') MAP2[i][j] = 3;
 			}
 		}
-		string cmd;		// 입력
+		string cmd;		// 방향입력
 		cin >> cmd;
 		for (int c = 0; c < cmd.length(); c++) {
 			if (cmd[c] == 'U') {
-				int ny = wy - 1;	
+				int ny = wy - 1;
 				if (ny < 0) continue;
 				if (MAP[ny][wx] == '#') continue;	// 다음 칸 벽이면 continue
 				if (MAP[ny][wx] == 'b' || MAP[ny][wx] == 'B') {		// 다음 칸 상자인 경우
 					if (MAP[ny - 1][wx] == 'b' || MAP[ny - 1][wx] == 'B' || MAP[ny - 1][wx] == '#') continue;	// 두 칸 뒤가 상자나 벽인 경우
-					if (MAP2[ny - 1][wx] == 3) MAP[ny - 1][wx] = 'B';
+					if (MAP2[ny - 1][wx] == 3) MAP[ny - 1][wx] = 'B';	// 상자가 밀릴 칸이 +면 'B'
 					else MAP[ny - 1][wx] = 'b';
 				}
-				if (MAP2[ny][wx] == 3) MAP[ny][wx] = 'W';
+				if (MAP2[ny][wx] == 3) MAP[ny][wx] = 'W';	// 캐릭터가 이동할 칸이 +면 'W'
 				else MAP[ny][wx] = 'w';
-				if (MAP2[wy][wx] == 3) {
+				if (MAP2[wy][wx] == 3) {		// 원래 캐릭터 위치가 +면 +
 					MAP[wy][wx] = '+';
 				}
 				else MAP[wy][wx] = '.';
@@ -104,12 +106,7 @@ int main()
 				else MAP[wy][wx] = '.';
 				wx = nx;
 			}
-			cout << "\n==========\n";
-			cout << cmd[c]<<"\n";
-			for (int i = 0; i < R; i++) {
-				cout << MAP[i] << "\n";
-			}
-			cout << "\n";
+
 			int flag = 1;
 			for (int i = 0; i < R; i++) {
 				for (int j = 0; j < C; j++) {
@@ -121,13 +118,13 @@ int main()
 				if (flag == 0) break;
 			}
 			if (flag == 1) {
-				flag2 = 1;
+				endgame = 1;
 				break;
 			}
 		}
 
 		cout << "Game " << t;
-		if (flag2 == 0) {
+		if (endgame == 0) {
 			cout << ": incomplete\n";
 		}
 		else {
@@ -138,7 +135,5 @@ int main()
 		}
 		t++;
 	}
-
-
 	return 0;
 }
