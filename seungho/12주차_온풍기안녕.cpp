d45d06082bdf33e0083583fceee36286f1dd0153
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2022-11-10 01:25:53 +0900

    Update 12주차_온풍기안녕.cpp

--- seungho/12주차_온풍기안녕.cpp
+++ seungho/12주차_온풍기안녕.cpp
@@ -14,49 +14,95 @@ int wall[21][21][2];
 vector<Node> v;
 vector<Node> check;
 
-int diryR[3] = { -1,0,1 };
-int dirxR[3] = { 1, 1, 1 };
+// 바람 퍼지는 방향배열
+int diry[4][3] = {
+	{-1,0,1},	// 우
+	{-1,0,1},	// 좌
+	{-1,-1,-1},	// 상
+	{1,1,1}		// 하
+};
+int dirx[4][3] = {
+	{1,1,1},
+	{-1,-1,-1},
+	{-1,0,1},
+	{-1,0,1}
+};
 
-int mdiryR[3] = {-1,0, 1};
-int mdirxR[3] = { 0,1, 0};
+// 온풍기 바로 옆칸 구하는 방향배열
+int hdiry[4] = { 0,0,-1,1 };
+int hdirx[4] = { 1,-1,0,0 };
+
+// 벽으로 바람이 막히는지 구하는 방향배열
+
+int wdiry[4][3][2] = {
+	{{-1,-1},{0,10},{1,1}},
+	{{-1,-1},{0,10},{1,1}},
+	{{0,-1},{-1,10},{0,1}},
+	{{0,-1},{1,10},{0,1}}
+};
+int wdirx[4][3][2] = {
+	{{0,1},{1,10},{0,1}},
+	{{0,-1},{-1,10},{0,-1}},
+	{{-1,-1},{0,10},{1,-1}},
+	{{-1,1},{0,10},{1,1}}
+};
+
 
 void bfs(int y, int x, int t) {
 	int visited[21][21] = { 0 };
 
 	queue<Node> q;
-	// 오른쪽 방향 온풍기인 경우
-	q.push({ y,x+1,t });
-	visited[y][x+1] = 5;
+	int fy = y + hdiry[t];
+	int fx = x + hdirx[t];
+	if (fy < 0 || fx < 0 || fy >= R || fx >= C) return;
+	q.push({ y+hdiry[t],x + hdirx[t],t});
+	visited[fy][fx] = 5;
 
-	for (int a = 0; a < 3; a++) {
+	while (!q.empty()) {
 		Node now = q.front();
 		q.pop();
-		if (visited[now.y][now.x] == 1) break;	// 1도 올라간 칸부터는 더 퍼지지 않는다.
-		int ny = now.y + diryR[3];
-		int nx = now.x + dirxR[3];
-		if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
-		if (MAP[ny][nx] > 0 && MAP[ny][nx] < 5) continue;	// 온풍기 있는 칸이면 continue
+		for (int a = 0; a < 3; a++) {
+			if (visited[now.y][now.x] == 1) break;	// 1도 상승하는 칸부터는 더 퍼지지 않는다.
+			int ny = now.y + diry[t][3];
+			int nx = now.x + dirx[t][3];
+			if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+			if (MAP[ny][nx] > 0 && MAP[ny][nx] < 5) continue;	// 온풍기 있는 칸이면 continue
 
-		// 벽이 있으면 continue
-		int flag = 0;
-		switch (a) {
-		case(0):
-			if (wall[now.y][now.x][0] == 1) flag = 1;
-			else if (wall[now.y - 1][now.x][1] == 1) flag = 1;
-			break;
-		case(1):
-			if (wall[now.y][now.x][1] == 1) flag = 1;
-			break;
-		case(2):
-			if (wall[now.y + 1][now.x][0] == 1) flag = 1;
-			else if (wall[now.y + 1][now.x][1] == 1) flag = 1;
-			break;
+			// 벽이 있으면 continue
+			int flag = 0;
+			for (int d = 0; d < 2; d++) {
+				if (a == 1 && d == 1) continue;
+				int wy = now.y + wdiry[t][a][d];
+				int wx = now.x + wdirx[t][a][d];
+				//if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+
+				if(wall[wy][wx])
+			}
+			switch (a) {
+			case(0):
+				if (wall[now.y][now.x][0] == 1) flag = 1;
+				else if (wall[now.y - 1][now.x][1] == 1) flag = 1;
+				break;
+			case(1):
+				if (wall[now.y][now.x][1] == 1) flag = 1;
+				break;
+			case(2):
+				if (wall[now.y + 1][now.x][0] == 1) flag = 1;
+				else if (wall[now.y + 1][now.x][1] == 1) flag = 1;
+				break;
+			}
+			if (flag == 1) continue;
+
+			if (visited[ny][nx] > 0) continue;
+
+			q.push({ ny,nx,t });
+			visited[ny][nx] = visited[now.y][now.x] - 1;
 		}
-
-		if (visited[ny][nx] > 0) continue;
-
-		q.push({ ny,nx,t });
-		visited[ny][nx] = visited[now.y][now.x] - 1;
+	}
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			temp[i][j] += visited[i][j];
+		}
 	}
 }
 
@@ -105,39 +151,32 @@ int main()
 
 		// 모든 온풍기에서 바람
 		for (int m = 0; m < v.size(); m++) {
-			bfs(v[m].y, v[m].x, v[m].t);
+			bfs(v[m].y, v[m].x, v[m].t-1);
 		}
 
-		int dirdy[2] = { 0,1 };
-		int dirdx[2] = { 1,0 };
+		int dirdy[2] = { -1,0 };
+		int dirdx[2] = { 0,1 };
 
-		int temp2[21][21];
 		// 온도 조절
+		int temp2[21][21];	// temp 복사
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
 				temp2[i][j] = temp[i][j];
 			}
 		}
-		for (int i = 0; i < R; i++) {
+		for (int i = R-1; i >= 0; i--) {
 			for (int j = 0; j < C; j++) {
 				for (int d = 0; d < 2; d++) {
 					int ny = i + dirdy[d];
 					int nx = j + dirdx[d];
 					if (ny >= R || nx >= C) continue;
-					
+
 					// 벽이 있으면 continue
-					if (d == 0 && wall[i][j][1] == 1) continue;
-					if (d == 1 && wall[i + 1][j][0] == 1) continue;
-					
-					int dif = abs(temp2[i][j] - temp2[ny][nx]);
-					if (temp2[i][j] > temp2[ny][nx]) {
-						temp[i][j] -= dif / 4;
-						temp[ny][nx] += dif / 4;
-					}
-					else {
-						temp[i][j] += dif / 4;
-						temp[ny][nx] -= dif / 4;
-					}
+					if (wall[i][j][d] == 1) continue;
+
+					int dif = temp2[i][j] - temp2[ny][nx];
+					temp[i][j] -= dif / 4;
+					temp[ny][nx] += dif / 4;
 				}
 			}
 		}
