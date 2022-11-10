0f1bea578e088badf83ff70d76e3ee7f84f7dae8
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2022-11-10 17:45:06 +0900

    Update 12주차_온풍기안녕.cpp

--- seungho/12주차_온풍기안녕.cpp
+++ seungho/12주차_온풍기안녕.cpp
@@ -55,18 +55,20 @@ void bfs(int y, int x, int t) {
 	int fy = y + hdiry[t];
 	int fx = x + hdirx[t];
 	if (fy < 0 || fx < 0 || fy >= R || fx >= C) return;
-	q.push({ y+hdiry[t],x + hdirx[t],t});
+	q.push({ fy, fx, t });
 	visited[fy][fx] = 5;
 
 	while (!q.empty()) {
 		Node now = q.front();
 		q.pop();
+		if (visited[now.y][now.x] == 1) break;	// 1도 상승하는 칸부터는 더 퍼지지 않는다.
+
 		for (int a = 0; a < 3; a++) {
-			if (visited[now.y][now.x] == 1) break;	// 1도 상승하는 칸부터는 더 퍼지지 않는다.
-			int ny = now.y + diry[t][3];
-			int nx = now.x + dirx[t][3];
+			int ny = now.y + diry[t][a];
+			int nx = now.x + dirx[t][a];
 			if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
 			if (MAP[ny][nx] > 0 && MAP[ny][nx] < 5) continue;	// 온풍기 있는 칸이면 continue
+			if (visited[ny][nx] > 0) continue;
 
 			// 벽이 있으면 continue
 			int flag = 0;
@@ -75,35 +77,114 @@ void bfs(int y, int x, int t) {
 				int wy = now.y + wdiry[t][a][d];
 				int wx = now.x + wdirx[t][a][d];
 				//if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+			}
+			if (t == 0) {
+				switch (a) {
+				case(0):
+					if (wall[now.y][now.x][0] == 1) flag = 1;
+					else if (wall[now.y - 1][now.x][1] == 1) flag = 1;
+					break;
+				case(1):
+					if (wall[now.y][now.x][1] == 1) flag = 1;
+					break;
+				case(2):
+					if (wall[now.y + 1][now.x][0] == 1) flag = 1;
+					else if (wall[now.y + 1][now.x][1] == 1) flag = 1;
+					break;
+				}
+			}
+			else if (t == 1) {
+				switch (a) {
+				case(0):
+					if (wall[now.y][now.x][0] == 1) flag = 1;
+					else if (wall[now.y - 1][now.x - 1][1] == 1) flag = 1;
+					break;
+				case(1):
+					if (wall[now.y][now.x - 1][1] == 1) flag = 1;
+					break;
+				case(2):
+					if (wall[now.y + 1][now.x][0] == 1) flag = 1;
+					else if (wall[now.y + 1][now.x - 1][1] == 1) flag = 1;
+					break;
+				}
+			}
+			else if (t == 2) {
+				switch (a) {
+				case(0):
+					if (wall[now.y][now.x - 1][1] == 1) flag = 1;
+					else if (wall[now.y][now.x - 1][0] == 1) flag = 1;
+					break;
+				case(1):
+					if (wall[now.y][now.x][0] == 1) flag = 1;
+					break;
+				case(2):
+					if (wall[now.y][now.x][1] == 1) flag = 1;
+					else if (wall[now.y][now.x + 1][0] == 1) flag = 1;
+					break;
+				}
+			}
+			else {
+				switch (a) {
+				case(0):
+					if (wall[now.y][now.x - 1][1] == 1) flag = 1;
+					else if (wall[now.y + 1][now.x - 1][0] == 1) flag = 1;
+					break;
+				case(1):
+					if (wall[now.y + 1][now.x][0] == 1) flag = 1;
+					break;
+				case(2):
+					if (wall[now.y][now.x][1] == 1) flag = 1;
+					else if (wall[now.y + 1][now.x + 1][0] == 1) flag = 1;
+					break;
+				}
+			}
 
-				if(wall[wy][wx])
-			}
-			switch (a) {
-			case(0):
-				if (wall[now.y][now.x][0] == 1) flag = 1;
-				else if (wall[now.y - 1][now.x][1] == 1) flag = 1;
-				break;
-			case(1):
-				if (wall[now.y][now.x][1] == 1) flag = 1;
-				break;
-			case(2):
-				if (wall[now.y + 1][now.x][0] == 1) flag = 1;
-				else if (wall[now.y + 1][now.x][1] == 1) flag = 1;
-				break;
-			}
 			if (flag == 1) continue;
-
-			if (visited[ny][nx] > 0) continue;
 
 			q.push({ ny,nx,t });
 			visited[ny][nx] = visited[now.y][now.x] - 1;
+			cout << "\n\nvisited: " << visited[ny][nx] << "\n\n";
+			cout << "\n---------------temp---------------\n";
+			for (int i = 0; i < R; i++) {
+				for (int j = 0; j < C; j++) {
+					cout << temp[i][j] << " ";
+				}
+				cout << "\n";
+			}
+
+			cout << "\n----------------------------------\n";
+
+			cout << "\n---------------visited---------------\n";
+			for (int i = 0; i < R; i++) {
+				for (int j = 0; j < C; j++) {
+					cout << visited[i][j] << " ";
+				}
+				cout << "\n";
+			}
+
+			cout << "\n----------------------------------\n";
+
+			int aaa = 1;
 		}
 	}
+
+	cout << "\n---------------visited 최종---------------\n";
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
-			temp[i][j] += visited[i][j];
+			cout << visited[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\n----------------------------------\n";
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cout << temp[i][j]<<","<<visited[i][j] << " ";
+			if (visited[i][j] > 0)	temp[i][j] = temp[i][j] + visited[i][j];
 		}
 	}
+	cout << "\n";
 }
 
 
@@ -118,11 +199,11 @@ int main()
 		for (int j = 0; j < C; j++) {
 			cin >> MAP[i][j];
 			if (MAP[i][j] == 0) continue;
-			if (MAP[i][j] == 5) {
+			else if (MAP[i][j] == 5) {
 				check.push_back({ i,j,5 });
 			}
 			else {
-				v.push_back({ i,j,MAP[i][j] });
+				v.push_back({ i,j,MAP[i][j] - 1 });
 			}
 		}
 	}
@@ -131,7 +212,7 @@ int main()
 	for (int i = 0; i < W; i++) {
 		int x, y, t;
 		cin >> x >> y >> t;
-		wall[x][y][t] = 1;
+		wall[x - 1][y - 1][t] = 1;
 	}
 
 	int chocolate = 0;
@@ -147,11 +228,12 @@ int main()
 				break;
 			}
 		}
+
 		if (flag == 0) break;
 
 		// 모든 온풍기에서 바람
 		for (int m = 0; m < v.size(); m++) {
-			bfs(v[m].y, v[m].x, v[m].t-1);
+			bfs(v[m].y, v[m].x, v[m].t);
 		}
 
 		int dirdy[2] = { -1,0 };
@@ -164,7 +246,7 @@ int main()
 				temp2[i][j] = temp[i][j];
 			}
 		}
-		for (int i = R-1; i >= 0; i--) {
+		for (int i = R - 1; i >= 0; i--) {
 			for (int j = 0; j < C; j++) {
 				for (int d = 0; d < 2; d++) {
 					int ny = i + dirdy[d];
@@ -173,10 +255,15 @@ int main()
 
 					// 벽이 있으면 continue
 					if (wall[i][j][d] == 1) continue;
-
 					int dif = temp2[i][j] - temp2[ny][nx];
-					temp[i][j] -= dif / 4;
-					temp[ny][nx] += dif / 4;
+					if (dif > 0) {
+						temp[i][j] -= abs(dif) / 4;
+						temp[ny][nx] += abs(dif) / 4;
+					}
+					else {
+						temp[i][j] += abs(dif) / 4;
+						temp[ny][nx] -= abs(dif) / 4;
+					}
 				}
 			}
 		}
@@ -195,7 +282,7 @@ int main()
 		chocolate++;
 		if (chocolate > 100) {
 			cout << 101;
-			return;
+			return 0;
 		}
 	}
 
