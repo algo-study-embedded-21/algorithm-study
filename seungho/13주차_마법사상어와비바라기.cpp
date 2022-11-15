15f7a1ea0ee3085fb950969194f5a04ee68793bd
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2022-11-15 10:27:20 +0900

    Update 13주차_마법사상어와비바라기.cpp

--- seungho/13주차_마법사상어와비바라기.cpp
+++ seungho/13주차_마법사상어와비바라기.cpp
@@ -6,16 +6,13 @@ int N, M;
 int MAP[50][50];
 
 struct Node {
-	int d; int s;
+	int y; int x;
 };
 
 Node moves[100];
 
 queue<Node> cloud;
 queue<Node> rain;
-
-int diry[8] = {0,-1,-1,-1,0,1,1,1};
-int dirx[8] = {-1,-1,0,1,1,1,0,-1};
 
 int diry2[4] = { -1,-1,1,1 };
 int dirx2[4] = { -1,1,1,-1 };
@@ -32,6 +29,9 @@ int main()
 			cin >> MAP[i][j];
 		}
 	}
+	
+	int diry[8] = { 0,N - 1,N - 1,N - 1,0,1,1,1 };
+	int dirx[8] = { N - 1,N - 1,0,1,1,1,0,N - 1 };
 
 	// 0. 비구름 생성
 	cloud.push({ N - 1,0 });
@@ -45,38 +45,39 @@ int main()
 
 		int cloudMAP[50][50]={0};
 
-		// 1. 모든 구름이 di 방향으로 si칸 이동
+		// 1. 모든 구름이 d 방향으로 s칸 이동
 		// -> cloud에서 rain으로 이동
 		while (!cloud.empty()) {
 			Node now = cloud.front();
-			int ny = now.d + s * diry[d-1];
-			int nx = now.s + s * dirx[d-1];
+			int ny = now.y + s * diry[d-1];
+			int nx = now.x + s * dirx[d-1];
 			ny %= N;
 			nx %= N;
 			rain.push({ ny,nx });
 
-			cloudMAP[ny][nx] = 1; // 이번 턴에 구름이 있었던 곳 체크
-
 			// 2. 각 구름에서 비
 			MAP[ny][nx]++;
+
+
 			// 3. 구름이 모두 사라진다.
 			cloud.pop();
+			cloudMAP[ny][nx] = 1; // 이번 턴에 구름이 사라진 칸 체크
 		}
 		// 4. 2에서 물이 증가한 칸에 물복사버그
 		while(!rain.empty()){
 			Node now = rain.front();
 			rain.pop();
-			int cnt = 0;
-
+			
 			// 대각선 방향에 물이 있는 바구니 수 count
+			int cnt = 0;
 			for (int r = 0; r < 4; r++) {
-				int ny = now.d + diry2[r];
-				int nx = now.s + dirx2[r];
+				int ny = now.y + diry2[r];
+				int nx = now.x + dirx2[r];
 				if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
 				if (MAP[ny][nx] == 0) continue;
 				cnt++;
 			}
-			MAP[now.d][now.s] += cnt;
+			MAP[now.y][now.x] += cnt;
 		}
 
 		// 5. 물의 양 2 이상인 모든 칸에 구름
@@ -85,6 +86,7 @@ int main()
 				if (cloudMAP[i][j]) continue;
 				if (MAP[i][j] >= 2) {
 					cloud.push({ i,j });
+					MAP[i][j] -= 2;
 				}
 			}
 		}
