03be5baad2b0028aa0a039642723b104d28e5b57
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2022-11-17 12:34:14 +0900

    Update 13주차_배열돌리기6.cpp

--- seungho/13주차_배열돌리기6.cpp
+++ seungho/13주차_배열돌리기6.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int N,R;
+int N, R;
 int MAP[300][300];
 int MAP2[300][300];
 int msize;
 int bsize;
 
 void one() {
-	for (int i = 0; i < msize/bsize; i++) {
-		for (int j = 0; j < msize/bsize; j++) {
+	for (int i = 0; i < msize / bsize; i++) {
+		for (int j = 0; j < msize / bsize; j++) {
 			for (int bi = 0; bi < bsize; bi++) {
 				for (int bj = 0; bj < bsize; bj++) {
-					MAP[i * bsize + bi][j * bsize + bj] = MAP2[i * bsize + (bsize-1-bi)][j * bsize + bj];
+					MAP[i * bsize + bi][j * bsize + bj] = MAP2[i * bsize + (bsize - 1 - bi)][j * bsize + bj];
 				}
 			}
 		}
@@ -24,7 +25,7 @@ void two() {
 		for (int j = 0; j < msize / bsize; j++) {
 			for (int bi = 0; bi < bsize; bi++) {
 				for (int bj = 0; bj < bsize; bj++) {
-					MAP[i * bsize + bi][j * bsize + bj] = MAP2[i * bsize + bi][j * bsize + (bsize-1-bj)];
+					MAP[i * bsize + bi][j * bsize + bj] = MAP2[i * bsize + bi][j * bsize + (bsize - 1 - bj)];
 				}
 			}
 		}
@@ -35,7 +36,7 @@ void three() {
 		for (int j = 0; j < msize / bsize; j++) {
 			for (int bi = 0; bi < bsize; bi++) {
 				for (int bj = 0; bj < bsize; bj++) {
-					MAP[j * bsize + bj][i * bsize + (bsize-1-bi)] = MAP2[i * bsize + bi][j * bsize + bj];
+					MAP[i * bsize + bj][j * bsize + (bsize - 1 - bi)] = MAP2[i * bsize + bi][j * bsize + bj];
 				}
 			}
 		}
@@ -46,7 +47,7 @@ void four() {
 		for (int j = 0; j < msize / bsize; j++) {
 			for (int bi = 0; bi < bsize; bi++) {
 				for (int bj = 0; bj < bsize; bj++) {
-					MAP[j * bsize + (bsize-1-bj)][i * bsize + bi] = MAP2[i * bsize + bi][j * bsize + bj];
+					MAP[i * bsize + (bsize - 1 - bj)][j * bsize + bi] = MAP2[i * bsize + bi][j * bsize + bj];
 				}
 			}
 		}
@@ -58,7 +59,7 @@ void five() {
 		for (int j = 0; j < msize / bsize; j++) {
 			for (int bi = 0; bi < bsize; bi++) {
 				for (int bj = 0; bj < bsize; bj++) {
-					MAP[bsize*(msize/bsize -1 -i)+bi][j*bsize+bj] = MAP2[i*bsize+bi][j*bsize+bj];
+					MAP[bsize * (msize / bsize - 1 - i) + bi][j * bsize + bj] = MAP2[i * bsize + bi][j * bsize + bj];
 				}
 			}
 		}
@@ -69,17 +70,33 @@ void six() {
 		for (int j = 0; j < msize / bsize; j++) {
 			for (int bi = 0; bi < bsize; bi++) {
 				for (int bj = 0; bj < bsize; bj++) {
-					MAP[i*bsize+bi][bsize * (msize / bsize - 1 - j) + bj] = MAP2[i * bsize + bi][j * bsize + bj];
+					MAP[i * bsize + bi][bsize * (msize / bsize - 1 - j) + bj] = MAP2[i * bsize + bi][j * bsize + bj];
 				}
 			}
 		}
 	}
 }
 void seven() {
-
+	for (int i = 0; i < msize / bsize; i++) {
+		for (int j = 0; j < msize / bsize; j++) {
+			for (int bi = 0; bi < bsize; bi++) {
+				for (int bj = 0; bj < bsize; bj++) {
+					MAP[j * bsize + bi][(msize / bsize - 1 - i) * bsize + bj] = MAP2[i * bsize + bi][j * bsize + bj];
+				}
+			}
+		}
+	}
 }
 void eight() {
-
+	for (int i = 0; i < msize / bsize; i++) {
+		for (int j = 0; j < msize / bsize; j++) {
+			for (int bi = 0; bi < bsize; bi++) {
+				for (int bj = 0; bj < bsize; bj++) {
+					MAP[(msize / bsize - 1 - j) * bsize + bi][i * bsize + bj] = MAP2[i * bsize + bi][j * bsize + bj];
+				}
+			}
+		}
+	}
 }
 
 int main()
@@ -89,11 +106,11 @@ int main()
 	cout.tie();
 
 	cin >> N >> R;
-	
+
 	msize = pow(2, N);
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	for (int i = 0; i < msize; i++) {
+		for (int j = 0; j < msize; j++) {
 			cin >> MAP[i][j];
 		}
 	}
@@ -110,7 +127,7 @@ int main()
 			}
 		}
 
-		switch(k) {
+		switch (k) {
 		case(1):
 			one();
 			break;
@@ -136,11 +153,10 @@ int main()
 			eight();
 			break;
 		}
-
 	}
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	for (int i = 0; i < msize; i++) {
+		for (int j = 0; j < msize; j++) {
 			cout << MAP[i][j] << " ";
 		}
 		cout << "\n";
