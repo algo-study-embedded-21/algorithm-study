0b33b96cf075ffaa6886c2dfc9c76282b3d63fec
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2022-11-17 17:57:37 +0900

    Update 13주차_어른상어.cpp

--- seungho/13주차_어른상어.cpp
+++ seungho/13주차_어른상어.cpp
@@ -19,6 +19,9 @@ int shark[400];
 //상어 방향 우선순위
 int direction[400][4][4];
 
+int diry[4] = { -1,1,0,0 };
+int dirx[4] = { 0,0,-1,1 };
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -36,10 +39,13 @@ int main()
 	for (int i = 0; i < M; i++) {
 		cin >> shark[i];
 	}
+
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < 4; j++) {
 			for (int k = 0; k < 4; k++) {
-				cin >> direction[i][j][k];
+				int d;
+				cin >> d;
+				direction[i][j][k] = d-1;
 			}
 		}
 	}
@@ -58,14 +64,49 @@ int main()
 				if (MAP[i][j]) {
 					MAP2[i][j] = MAP[i][j];
 					scent[i][j] = { MAP[i][j],k };
+					MAP[i][j] = 0;
 				}
 			}
 		}
 
 		// 인접한 칸으로 상어 이동
-		
-
 		// 우선순위에 따라 방향 결정
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				int num = MAP2[i][j];
+				int dir = shark[MAP2[i][j]];
+				int flag = 0;
+				if (num > 0) {
+					// 아무 냄새 없는 칸으로 이동
+					for (int d = 0; d < 4; d++) {
+						int dnow = direction[num][dir][d];
+						int ny = i + diry[dnow];
+						int nx = j + dirx[dnow];
+						if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+						if (scent[ny][nx].t > 0) continue;
+						MAP[ny][nx] = num;
+						shark[num] = dnow;
+						flag = 1;
+						break;
+					}
+					if (flag == 1) break;
+					// 자신의 냄새 있는 칸으로 이동
+					for (int d = 0; d < 4; d++) {
+						int dnow = direction[num][dir][d];
+						int ny = i + diry[dnow];
+						int nx = j + dirx[dnow];
+						if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+						if (scent[ny][nx].n == num) {
+							MAP[ny][nx] = num;
+							shark[num] = dnow;
+							flag = 1;
+							break;
+						}
+					}
+				}
+			}
+		}
+		
 		// 이동할 칸에 이미 상어가 있는 경우 가장 작은 번호 가진 상어만 남는다.
 		// 결정된 방향으로 이동
 		
