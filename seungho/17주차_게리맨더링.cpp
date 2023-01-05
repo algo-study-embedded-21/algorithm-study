ff8ebf8fdfa37807c062c5eed79d9d8d35f1b56b
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2023-01-05 13:10:40 +0900

    Update 17주차_게리맨더링.cpp

--- seungho/17주차_게리맨더링.cpp
+++ seungho/17주차_게리맨더링.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int N;
@@ -10,6 +11,7 @@ int ans = 2134567890;
 
 // Union Find
 int parent[11];
+int groupCnt[11];
 
 int Find(int node) {
 	if (node == parent[node]) return node;
@@ -21,49 +23,38 @@ void Union(int a, int b) {
 	int pb = Find(b);
 	if (pa == pb) return;
 	parent[pb] = pa;
+	groupCnt[pa] += groupCnt[pb];
+	groupCnt[pb] = 0;
 }
 
 void dfs(int level) {
 	if (level > N) {
-		for (int i = 1; i <= N; i++) {
-
-		}
-		return;
-	}
-
-	for (int i = 1; i < 3; i++) {
-		Group[level] = i;
-		dfs(level + 1);
-	}
-}
-
-void dfs(int level) {
-	if (level > N) {
-		int cntGroup[3] = {0};
-		int GrpPop[3] = { 0 };
-
 		// 가능한 방법인지 확인
 		for (int i = 1; i <= N; i++) {
-			GrpPop[Group[i]] += population[i];
+			parent[i] = i;
+		}
+		memset(groupCnt, 1, sizeof(groupCnt));
+		int grpPop[3] = { 0 };
+		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
-				if (!MAP[i][j]) continue;	// 두 선거구가 연결되어 있지 않으면 continue
-				if (j == i) continue;		// 같은 번호이면 continue
-				
-				// 두 선거구가 연결되어 있고 같은 구역으로 나눠진 경우
-				if (Group[i] == Group[j]) {	
-					cntGroup[Group[i]]++;
-					break;
+				if (MAP[i][j] && Group[i] == Group[j]) {
+					Union(i, j);
 				}
 			}
+			grpPop[Group[i]] += population[i];
 		}
-		if (cntGroup[1] + cntGroup[2] >= N - 1) {
+		int cntgroups = 0;
+		for (int i = 1; i <= N; i++) {
+			if (groupCnt[i] > 0) cntgroups++;
+		}
+
+		if (cntgroups == 2) {
 			possible = 1;
-			ans = min(ans, abs(GrpPop[1] - GrpPop[2]));
+			ans = min(ans, abs(grpPop[1] - grpPop[2]));
 		}
 		return;
 	}
-
-	for (int i = 1; i <= 2; i++) {
+	for (int i = 1; i < 3; i++) {
 		Group[level] = i;
 		dfs(level + 1);
 	}
@@ -79,8 +70,6 @@ int main()
 		cin >> population[i];
 	}
 
-	if (N == 2) return abs(population[1] - population[2]);
-
 	for (int i = 1; i <= N; i++) {
 		int num;
 		cin >> num;
@@ -91,8 +80,13 @@ int main()
 			MAP[i][area] = 1;
 		}
 	}
-	
-	dfs(1);
+	if (N == 2) {
+		cout << abs(population[1] - population[2]);
+		return 0;
+	}
 
+	dfs(1);
+	if (possible) cout << ans;
+	else cout << -1;
 	return 0;
 }
