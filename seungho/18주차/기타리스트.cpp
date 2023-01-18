aa199928fbacff4701708feffb15d06fcf08ac3e
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2023-01-18 18:51:44 +0900

    Update 기타리스트.cpp

--- seungho/18주차/기타리스트.cpp
+++ seungho/18주차/기타리스트.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	int N, S, M;
 	int V[51];
-	int dp[51];
+	int dp[51][1001];
 
 	cin >> N >> S >> M;
 
@@ -14,9 +14,16 @@ int main()
 		cin >> V[i];
 	}
 	
-	dp[0] = S;
+	dp[0][S] = 1;
+        if(S+V[1]<=M) dp[1][S+V[1]] = 1;
+        if(S-V[1]>=0) dp[1][S-V[1]] = 1;
 
 	for (int i = 2; i <= N; i++) {
+            {
+                for(int j=0;j<=M;j++){
+                    if(dp[i-1][j]){
+                        if(j + V[i] <= M) dp[i][j+V[i]] = 1;
+                        if(j - V[i] >= 0) dp[i][j-V[i]] = 1;
 		if (dp[i - 1] + V[i] <= M) dp[i] = dp[i - 1] + V[i];
 		else if (dp[i - 1] - V[i] >= 0) dp[i] = max(dp[i], dp[i - 1] + V[i]);
 		else if(dp[i-2])
