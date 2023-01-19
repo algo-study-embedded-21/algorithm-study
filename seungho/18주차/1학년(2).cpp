0edc1b9e008712666769dc5addbb61fa98dad470
Seungho Shin <93854477+snghshn@users.noreply.github.com>
2023-01-19 11:53:23 +0900

    Update 1학년(2).cpp

--- seungho/18주차/1학년(2).cpp
+++ seungho/18주차/1학년(2).cpp
@@ -12,15 +12,15 @@ int main()
 	for (int i = 1; i <= N; i++) {
 		cin >> numbers[i];
 	}
-	if(numbers[1] + numbers[2] >= 0 && numbers[1] + numbers[2] <=20) dp[3][numbers[1] + numbers[2]] = 1;
-	if(numbers[1] - numbers[2] >= 0 && numbers[1] - numbers[2] <= 20) dp[3][numbers[1] - numbers[2]] = 1;
+	if(numbers[1] + numbers[2] <= 20) dp[3][numbers[1] + numbers[2]] += 1;
+	if(numbers[1] - numbers[2] >= 0) dp[3][numbers[1] - numbers[2]] += 1; 
 
 	for (int i = 4; i <= N; i++) {
 		for (int j = 0; j <= 20; j++) {
 			int x = j + numbers[i - 1];
 			int y = j - numbers[i - 1];
-			if(x >=0 && x <=20) dp[i][j] += dp[i - 2][x];
-			if (y >= 0 && y <= 20) dp[i][j] += dp[i - 2][y];
+			if(j + numbers[i - 1] <= 20) dp[i][j] += dp[i - 1][j + numbers[i - 1]];
+			if (j - numbers[i - 1] >= 0) dp[i][j] += dp[i - 1][j - numbers[i - 1]];
 		}
 	}
 	cout << dp[N][numbers[N]];
