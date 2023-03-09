83e70c6607d505cee3ae25cb0c7dfc0bd7eccc15
김준우 <84614315+junwook11@users.noreply.github.com>
2023-03-09 17:58:07 +0900

    Create 1062_가르침(미완).cpp

---
+++ junwoo/1062_가르침(미완).cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int N, K;
+vector<string> v;
+string base = "antic";
+int maxval = -2134567890;
+bool alpha[27];
+bool used[51];
+
+void setA() {
+	for (int i = 0; i < base.length(); i++) {
+		alpha[base[i] - 97] = true;
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		sort(v[i].begin(), v[i].end());
+		v[i].erase(unique(v[i].begin(), v[i].end()), v[i].end());
+	}
+
+}
+
+void input() {
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		string s;
+		cin >> s;
+
+		v.push_back(s.substr(4,s.length()-8));
+	}
+}
+
+void check(int lvl, int cnt) {
+	if (cnt == K - 5) {
+		return;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (alpha[i]) continue;
+
+		alpha[i] = true;
+		check(lvl, cnt + 1);
+		alpha[i] = false;
+	}
+
+}
+
+int main() {
+	input();
+	if (K < 5) {
+		cout << 0;
+		return 0;
+	}
+	setA();
+	check();
+	cout << maxval;
+}
